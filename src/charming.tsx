bbb707008965638569b6debd637067fbc48e69d6
Joe Hsu <jhsu.x1@gmail.com>
2018-02-16 11:39:00 -0800

    initial commit

---
+++ src/charming.tsx
@@ -0,0 +1,30 @@
+import * as React from 'react';
+
+interface RenderFunction {
+  (letters: any[]): any
+}
+
+export interface CharmingProps {
+  className: string;
+  letters: string;
+  render: RenderFunction;
+}
+
+class Charming extends React.Component<CharmingProps, {}> {
+  static defaultProps = {
+    letters: '',
+  };
+
+  render() {
+    const { className, letters, render } = this.props;
+    const ltrs = letters.split('');
+    const letterViews: any[] = ltrs.map((ltr: string, idx: number) => {
+      return (<span className={`${className}-${idx}`} aria-hidden>{ltr}</span>);
+    });
+    return <div aria-label={letters} className={className}>
+      {render(letterViews)}
+    </div>;
+  }
+}
+
+export default Charming;
