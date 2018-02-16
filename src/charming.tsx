f10de9f77fdda5c3ccc86c382bebf4477a6ee72e
Joe Hsu <jhsu.x1@gmail.com>
2018-02-16 12:54:29 -0800

    add key

--- src/charming.tsx
+++ src/charming.tsx
@@ -20,7 +20,7 @@ class Charming extends React.Component<CharmingProps, {}> {
     const { className, letters, render } = this.props;
     const ltrs = letters.split('');
     const letterViews: any[] = ltrs.map((ltr: string, idx: number) => {
-      return (<span className={`${className}-${idx}`} aria-hidden>{ltr}</span>);
+      return (<span key={idx} className={`${className}-${idx}`} aria-hidden>{ltr}</span>);
     });
     return <div aria-label={letters} className={className}>
       {render(letterViews)}
